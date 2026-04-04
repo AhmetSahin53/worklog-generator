188cc1e9f95187b94bcd7a22c7148fbb4c73551f
GitHub Action <action@github.com>
2026-04-04 11:04:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/ahmetsahin53/workloggenerator/MyBundle.kt
+++ src/main/kotlin/com/github/ahmetsahin53/workloggenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ahmetsahin53.workloggenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ahmetsahin53/workloggenerator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ahmetsahin53/workloggenerator/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ahmetsahin53.workloggenerator.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ahmetsahin53.workloggenerator.MyBundle
+import com.github.ahmetsahin53.workloggenerator.services.MyProjectService
 import javax.swing.JButton
 
 
